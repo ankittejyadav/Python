1e679dda106cb66f963ae89e06d04f38255a673c
Ankit Yadav <ankittejyadav@gmail.com>
2024-05-12 13:23:19 -0500

    unpacking

--- unpacking.py
+++ unpacking.py
@@ -1,3 +1,29 @@
-values=[1,2,3,4,5,6]
-# list -> int
-print(values)
+values = [1, 2, 3, 4, 5, 6]
+# list-> int
+print("These", "are", values)
+
+print(*values)
+print(1, 2, 3, 4, 5, 6)
+
+
+def my_sum(a, b, c):
+    return a + b + c
+
+
+values = (1, 2, 3)
+print(my_sum(*values))
+print(my_sum(values[0], values[1], values[2]))
+v1, v2, v3 = values
+print(v1, v2, v3)
+
+
+values = (1, 2, 3, 4, 5, 6)
+v1, *v2, v3 = values
+print(v1, v2, v3)
+
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+combined = [*list1, "test", *list2]
+combined2 = [1, 2, 3, "test", 4, 5, 6]
+print(combined)
+print(combined2)
