6192a75e103ac881291211f5bbf4dcde22aea15a
Ankit Yadav <ankittejyadav@gmail.com>
2024-10-11 09:35:06 -0400

    pytest with paraeterization

--- pytest/phonebook.py
+++ pytest/phonebook.py
@@ -1,14 +1,15 @@
 from dataclasses import dataclass, field
 import os
+from pathlib import Path
 
 
 @dataclass
 class Phonebook:
     numbers: dict = field(default_factory=dict)
 
-    def __init__(self):
+    def __init__(self, temp_dir):
         self.numbers = {}
-        self.filename = "phonebook.txt"
+        self.filename = Path(temp_dir) / "phonebook.txt"
         self.cache = open(self.filename, "w")
 
     def add_empty(self, name: str, number: str) -> None:
@@ -29,3 +30,12 @@ class Phonebook:
     def clear(self):
         self.cache.close()
         os.remove(self.filename)
+
+    def is_consistent(self):
+        for name1, number1 in self.numbers.items():
+            for name2, number2 in self.numbers.items():
+                if name1 == name2:
+                    continue
+                if number1.startswith(number2):
+                    return False
+        return True
--- pytest/test_phonebook.py
+++ pytest/test_phonebook.py
@@ -3,21 +3,17 @@ import pytest
 
 
 @pytest.fixture
-def phonebook():
-    phonebook = Phonebook()
+def phonebook(tmpdir):
+    "Provides an empty phonebook"
+    phonebook = Phonebook(tmpdir)
     yield phonebook
     phonebook.clear()
 
 
-def test_lookup_by_name() -> None:
-    phonebook1 = Phonebook()
-    phonebook1.add_empty("Ankit", "123456")
-    number = phonebook1.lookup_empty("Ankit")
-    # assert number == "123456"  # Fails
-
-    phonebook2 = Phonebook()
-    phonebook2.add("Ankit", "123456")
-    number = phonebook2.lookup("Ankit")
+def test_lookup_by_name(phonebook) -> None:
+    # phonebook = Phonebook()
+    phonebook.add("Ankit", "123456")
+    number = phonebook.lookup("Ankit")
     assert number == "123456"
 
 
@@ -34,3 +30,35 @@ def test_name_not_present(phonebook) -> None:
     # phonebook.add("Bob", "123456")
     with pytest.raises(KeyError):
         phonebook.lookup("Bob")
+
+
+def test_phonebook_is_consistent_with_different_entries(phonebook):
+    phonebook.add("Ankit", "12345")
+    phonebook.add("Varun", "67890")
+    assert phonebook.is_consistent()
+
+
+def test_phonebook_inconsistent_with_same_entries(phonebook):
+    phonebook.add("Ankit", "12345")
+    phonebook.add("Varun", "12345")
+    assert not phonebook.is_consistent()
+
+
+def test_phonebook_inconsistent_with_same_prefix(phonebook):
+    phonebook.add("Ankit", "1234567890")
+    phonebook.add("Varun", "12345")
+    assert not phonebook.is_consistent()
+
+
+@pytest.mark.parametrize(
+    "entry1,entry2,is_consistent",
+    [
+        (("Ankit", "12345"), ("Varun", "67890"), True),
+        (("Ankit", "12345"), ("Varun", "12345"), False),
+        (("Ankit", "1234567890"), ("Varun", "12345"), False),
+    ],
+)
+def test_is_consistent(phonebook, entry1, entry2, is_consistent):
+    phonebook.add(*entry1)
+    phonebook.add(*entry2)
+    assert phonebook.is_consistent() == is_consistent
